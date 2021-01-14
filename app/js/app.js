ebe016768457e1759b100af1a658a70e64f24035
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-01-14 23:04:14 +0300

    add services

--- app/js/app.js
+++ app/js/app.js
@@ -16,4 +16,53 @@ $(function() {
 	}).bind('closed', function() {
 		$('.hamburger').removeClass('is-active');
 	});
+
+	$('.carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function() {
+			carousel-services()
+		}, 100);	
+	});
+	$('.carousel-services').owlCarousel({
+		loop: true, //зацикленная
+		nav: true,
+		smartSpeed: 700, //скорость
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}
+	});
+
+	$('.carousel-services-content').equalHeights();
+
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths = $(this),
+					thsh = ths.find('.carousel-services-content').outerHeight();
+					ths.find('.carousel-services-image').css('min-height', thsh);
+		});
+	}carouselService();
+
+	$('.carousel-services-composition .h3').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+
+	// Resize Window
+	function onResize() {
+		$('.carousel-services-content').equalHeights();
+	}
+	onResize();
+
+	window.onresize = function() {onResize()};
+
 });
