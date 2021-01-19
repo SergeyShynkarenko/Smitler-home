06aef922c39b556117069cc160d8f6fca5fda9ed
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-01-19 23:39:13 +0300

    add partners slider (html)

--- app/js/app.js
+++ app/js/app.js
@@ -98,4 +98,27 @@ $(function() {
 		autoHeight: true
 	});
 
+	$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
 });
